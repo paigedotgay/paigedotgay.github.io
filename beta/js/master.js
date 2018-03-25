2384c77d1bf17b4f2c0cb6b1bccc0d490a287f06
paige <qanazoga@gmail.com>
2018-03-24 22:50:42 -0600

    fixed hamburger not going away on animation

--- beta/js/master.js
+++ beta/js/master.js
@@ -3,6 +3,7 @@ $('body').scrollspy({target: ".navbar", offset: 50});
 
 // Add smooth scrolling on all links inside the navbar
 $("#collapsible-navbar a").on('click',  function(event) {
+  $("#hamburger-button").click();
 
   // Make sure this.hash has a value before overriding default behavior
   if (this.hash !== "") {
